f1ff0776fc995ae09f961b93cb3e4a56e5ba7a51
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-04-14 15:24:20 +0300

    hey

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from "../../public/gencdemok.png"
+import React from "react";
+import Image from "next/image";
+import logo from "../../public/gencdemok.png";
+import imza from "../../public/demokimza.png"
 
 const Hero = () => {
   return (
-    <div className='flex pb-6 md:pb-12 space-y-5 text-white flex-col justify-center items-center' >
-      <h1 className=' font-bold text-4xl' >Genç Demokratlar Vakfı</h1>
-      <h3 className='text-2xl' >14 Mayıs 2024 tarihinde tekrar sizlerle</h3>
-      <Image src={logo} width={200} height="auto" alt='gençdemoklatlarvakfi' />
+    <div className="flex pb-6 md:pb-12 space-y-5 text-white flex-col justify-center items-center">
+      <h1 className=" font-bold text-2xl md:text-4xl">Genç Demokratlar Vakfı</h1>
+      <h3 className="text-lg md:text-xl italic">14 Mayıs 2024 tarihinde tekrar sizlerle</h3>
+      <Image src={logo} width={200} height="auto" alt="gençdemoklatlarvakfi" />
+      <Image src={imza} width="auto" height={90} alt="gençdemoklatlarvakfi" />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
