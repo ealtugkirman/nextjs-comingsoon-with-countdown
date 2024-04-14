d44b6f7f1ffbe80b18a990b1392db9bd2335f037
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-04-14 15:34:00 +0300

    hey

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/hero";
 import CountDown from "@/components/countdown";
 export default function Home() {
   return (
-    <main className="h-full px-8 md:px-0 w-full flex flex-col justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-900 to-black">
+    <main className="h-full px-2 md:px-0 w-full flex flex-col justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-900 to-black">
       <Hero />
       <CountDown />
     </main>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,9 +5,9 @@ import imza from "../../public/demokimza.png"
 
 const Hero = () => {
   return (
-    <div className="flex pb-6 md:pb-12 space-y-5 text-white flex-col justify-center items-center">
+    <div className="flex  pb-6 md:pb-12 space-y-5 text-white flex-col justify-center items-center">
       <h1 className=" font-bold text-2xl md:text-4xl">Genç Demokratlar Vakfı</h1>
-      <h3 className="text-lg md:text-xl">14 Mayıs 2024 tarihinde tekrar sizlerle</h3>
+      <h3 className="text-lg ">14 Mayıs 2024 tarihinde tekrar sizlerle</h3>
       <Image src={logo} width={200} height="auto" alt="gençdemoklatlarvakfi" />
       <Image src={imza} width="auto" height={90} alt="gençdemoklatlarvakfi" />
     </div>
